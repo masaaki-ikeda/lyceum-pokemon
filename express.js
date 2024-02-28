aab089886b2dbcd64e6388325b59d9243899aa41
Your Name <you@example.com>
2024-02-28 05:58:22 +0000

    add README

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import app from "~/server/utils/app";
 import router from "~/server/utils/router";
 
-//以下の設定でCORを許可する
+//以下の設定でCORSを許可する
 app.use(
   cors({ origin: process.env.FRONTEND_ORIGIN ?? "http://localhost:3000" }),
 );
